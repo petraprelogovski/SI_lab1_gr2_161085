0c9b39edff54c37e463311c9c87dabfb955717a2
petraprelogovski <petra.prelogovski@gmail.com>
2020-03-26 20:00:36 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,72 @@
-class Student {
+import java.util.ArrayList;
+
+public class Student {
 	String index;
 	String firstName;
 	String lastName;
-	List<Integer> grades = new Arraylist();
+	ArrayList<Integer> grades;
 
 	//TODO constructor
-
-	//TODO seters & getters
-
-	public double getAverage() {
-		//TODO
+	public Student(String index, String firstName, String lastName, ArrayList<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades =  grades;
 	}
 
-	public int ECTSCredits() {
+	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public ArrayList<Integer> getGrades() {
+		return grades;
+	}
+
+	public void setGrades(ArrayList<Integer> grades) {
+		this.grades =  grades;
+	}
+
+	public double getAverage(ArrayList <Integer> grades) {
 		//TODO
+		double vkupno=0.0;
+		for (int grade:grades)
+		{
+			vkupno+=grade;
+		}
+		return vkupno/ grades.size();
+	}
+
+	public int ECTSCredits(ArrayList<Integer> grades) {
+		//TODO
+		int predmeti =0;
+		for(int grade: grades)
+		{
+			if (grade>5 && grade<=10)
+			{
+				predmeti+=1;
+			}
+		}
+		return predmeti*6;
 	}
 }
